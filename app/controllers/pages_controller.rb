5b8fba1c6757218e8a12b1810c17365f8898a63a
Joe Webber <35155442+j-webber@users.noreply.github.com>
2025-04-14 16:18:52 -0400

    let's try this again

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+  allow_unauthenticated_access only: :home
+  def home
+    redirect_to dashboard_path if authenticated?
+  end
+
+  def dashboard
+  end
+end
